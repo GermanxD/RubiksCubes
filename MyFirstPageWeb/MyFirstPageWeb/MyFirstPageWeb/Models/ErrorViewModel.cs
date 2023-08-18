b9ee62ac0afc1525b3a19c8bd165fea98c716487
German Bustos Moreno <52357914+GermanxD@users.noreply.github.com>
2023-08-17 20:38:12 -0600

    first commit
    first commit

---
+++ MyFirstPageWeb/MyFirstPageWeb/MyFirstPageWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstPageWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
